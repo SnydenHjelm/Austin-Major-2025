2d39d457a819900f20ff6fbe2551f5fe8548ccc0
Neo <neogd35@gmail.com>
2025-04-17 21:03:11 +0200

    Did basically everything lol
    Index page + stage 1 + stage 2 + stage 3 + server

---
+++ scripts/stage-3.js
@@ -0,0 +1,225 @@
+async function addStage3Team() {
+    let team = document.querySelector("#stage3-controls #team").value;
+    let player1 = document.querySelector("#stage3-controls #player1").value;
+    let player2 = document.querySelector("#stage3-controls #player2").value;
+    let player3 = document.querySelector("#stage3-controls #player3").value;
+    let player4 = document.querySelector("#stage3-controls #player4").value;
+    let player5 = document.querySelector("#stage3-controls #player5").value;
+    
+    let obj = {
+        team: team,
+        player1: player1.split(", "),
+        player2: player2.split(", "),
+        player3: player3.split(", "),
+        player4: player4.split(", "),
+        player5: player5.split(", ")
+    }
+
+    let req = new Request("http://localhost:8000/stage3", optionsGen("POST", obj));
+    try {
+        let resp = await fetch(req);
+        if (resp.status === 200) {
+            document.querySelector("#stage3-controls #status").textContent = `${team} added successfully`;
+            document.querySelector("#stage3-controls #team").value = "";
+            document.querySelector("#stage3-controls #player1").value = "";
+            document.querySelector("#stage3-controls #player2").value = "";
+            document.querySelector("#stage3-controls #player3").value = "";
+            document.querySelector("#stage3-controls #player4").value = "";
+            document.querySelector("#stage3-controls #player5").value = "";
+            await displayStage3();
+        } else {
+            let reso = await resp.text();
+            if (resp.status === 409) {
+                document.querySelector("#stage3-controls #status").textContent = reso
+            } else {
+                document.querySelector("#stage3-controls #status").textContent = reso;
+            }
+            return;
+        }
+    } catch (e) {
+        document.querySelector("#stage2-controls #status").textContent = "Network error";
+        return;
+    }
+}
+
+async function flagFromCountry(country) {
+    let allFlags = flags();
+    let theFlag = allFlags.find((x) => x.country.toLowerCase() === country.toLowerCase());
+    return theFlag.flag;
+}   
+
+async function displayStage3() {
+    let req = new Request("http://localhost:8000/stage3");
+    let resp = await fetch(req);
+    let div = document.querySelector("#stage3-qualied-teams");
+    div.innerHTML = "";
+    if (resp.ok) {
+        let reso = await resp.json();
+        for (let team of reso) {
+            let teamDiv = document.createElement("div");
+            teamDiv.classList.add("team");
+            teamDiv.innerHTML = `<img title="${team.team}" src="../images/${team.team}.png">
+            <ul>
+            <li>${await flagFromCountry(team.player1[1])} ${team.player1[0]}</li>
+            <li>${await flagFromCountry(team.player2[1])} ${team.player2[0]}</li>
+            <li>${await flagFromCountry(team.player3[1])} ${team.player3[0]}</li>
+            <li>${await flagFromCountry(team.player4[1])} ${team.player4[0]}</li>
+            <li>${await flagFromCountry(team.player5[1])} ${team.player5[0]}</li>
+            </ul>`;
+            div.appendChild(teamDiv);
+        }
+    }
+}
+
+async function driver() {
+    await displayStage3();
+    document.querySelector("#stage3-controls button").addEventListener("click", addStage3Team);
+}
+
+function flags() {
+    return [
+        { country: "Afghanistan", flag: "🇦🇫" },
+        { country: "Albania", flag: "🇦🇱" },
+        { country: "Algeria", flag: "🇩🇿" },
+        { country: "Andorra", flag: "🇦🇩" },
+        { country: "Angola", flag: "🇦🇴" },
+        { country: "Argentina", flag: "🇦🇷" },
+        { country: "Armenia", flag: "🇦🇲" },
+        { country: "Australia", flag: "🇦🇺" },
+        { country: "Austria", flag: "🇦🇹" },
+        { country: "Azerbaijan", flag: "🇦🇿" },
+        { country: "Bahamas", flag: "🇧🇸" },
+        { country: "Bahrain", flag: "🇧🇭" },
+        { country: "Bangladesh", flag: "🇧🇩" },
+        { country: "Barbados", flag: "🇧🇧" },
+        { country: "Belarus", flag: "🇧🇾" },
+        { country: "Belgium", flag: "🇧🇪" },
+        { country: "Belize", flag: "🇧🇿" },
+        { country: "Benin", flag: "🇧🇯" },
+        { country: "Bhutan", flag: "🇧🇹" },
+        { country: "Bolivia", flag: "🇧🇴" },
+        { country: "Bosnia", flag: "🇧🇦" },
+        { country: "Botswana", flag: "🇧🇼" },
+        { country: "Brazil", flag: "🇧🇷" },
+        { country: "Bulgaria", flag: "🇧🇬" },
+        { country: "Burkina Faso", flag: "🇧🇫" },
+        { country: "Burundi", flag: "🇧🇮" },
+        { country: "Cambodia", flag: "🇰🇭" },
+        { country: "Cameroon", flag: "🇨🇲" },
+        { country: "Canada", flag: "🇨🇦" },
+        { country: "Chile", flag: "🇨🇱" },
+        { country: "China", flag: "🇨🇳" },
+        { country: "Colombia", flag: "🇨🇴" },
+        { country: "Costa Rica", flag: "🇨🇷" },
+        { country: "Croatia", flag: "🇭🇷" },
+        { country: "Cuba", flag: "🇨🇺" },
+        { country: "Cyprus", flag: "🇨🇾" },
+        { country: "Czech Republic", flag: "🇨🇿" },
+        { country: "Denmark", flag: "🇩🇰" },
+        { country: "Dominican Republic", flag: "🇩🇴" },
+        { country: "Ecuador", flag: "🇪🇨" },
+        { country: "Egypt", flag: "🇪🇬" },
+        { country: "El Salvador", flag: "🇸🇻" },
+        { country: "Estonia", flag: "🇪🇪" },
+        { country: "Ethiopia", flag: "🇪🇹" },
+        { country: "Finland", flag: "🇫🇮" },
+        { country: "France", flag: "🇫🇷" },
+        { country: "Georgia", flag: "🇬🇪" },
+        { country: "Germany", flag: "🇩🇪" },
+        { country: "Ghana", flag: "🇬🇭" },
+        { country: "Greece", flag: "🇬🇷" },
+        { country: "Guatemala", flag: "🇬🇹" },
+        { country: "Honduras", flag: "🇭🇳" },
+        { country: "Hungary", flag: "🇭🇺" },
+        { country: "Iceland", flag: "🇮🇸" },
+        { country: "India", flag: "🇮🇳" },
+        { country: "Indonesia", flag: "🇮🇩" },
+        { country: "Iran", flag: "🇮🇷" },
+        { country: "Iraq", flag: "🇮🇶" },
+        { country: "Ireland", flag: "🇮🇪" },
+        { country: "Israel", flag: "🇮🇱" },
+        { country: "Italy", flag: "🇮🇹" },
+        { country: "Jamaica", flag: "🇯🇲" },
+        { country: "Japan", flag: "🇯🇵" },
+        { country: "Jordan", flag: "🇯🇴" },
+        { country: "Kazakhstan", flag: "🇰🇿" },
+        { country: "Kenya", flag: "🇰🇪" },
+        { country: "Kuwait", flag: "🇰🇼" },
+        { country: "Kyrgyzstan", flag: "🇰🇬" },
+        { country: "Latvia", flag: "🇱🇻" },
+        { country: "Lebanon", flag: "🇱🇧" },
+        { country: "Lithuania", flag: "🇱🇹" },
+        { country: "Luxembourg", flag: "🇱🇺" },
+        { country: "Madagascar", flag: "🇲🇬" },
+        { country: "Malaysia", flag: "🇲🇾" },
+        { country: "Maldives", flag: "🇲🇻" },
+        { country: "Malta", flag: "🇲🇹" },
+        { country: "Mexico", flag: "🇲🇽" },
+        { country: "Moldova", flag: "🇲🇩" },
+        { country: "Monaco", flag: "🇲🇨" },
+        { country: "Mongolia", flag: "🇲🇳" },
+        { country: "Montenegro", flag: "🇲🇪" },
+        { country: "Morocco", flag: "🇲🇦" },
+        { country: "Mozambique", flag: "🇲🇿" },
+        { country: "Myanmar", flag: "🇲🇲" },
+        { country: "Nepal", flag: "🇳🇵" },
+        { country: "Netherlands", flag: "🇳🇱" },
+        { country: "New Zealand", flag: "🇳🇿" },
+        { country: "Nicaragua", flag: "🇳🇮" },
+        { country: "Nigeria", flag: "🇳🇬" },
+        { country: "North Korea", flag: "🇰🇵" },
+        { country: "North Macedonia", flag: "🇲🇰"},
+        { country: "Norway", flag: "🇳🇴" },
+        { country: "Oman", flag: "🇴🇲" },
+        { country: "Pakistan", flag: "🇵🇰" },
+        { country: "Panama", flag: "🇵🇦" },
+        { country: "Paraguay", flag: "🇵🇾" },
+        { country: "Peru", flag: "🇵🇪" },
+        { country: "Philippines", flag: "🇵🇭" },
+        { country: "Poland", flag: "🇵🇱" },
+        { country: "Portugal", flag: "🇵🇹" },
+        { country: "Qatar", flag: "🇶🇦" },
+        { country: "Romania", flag: "🇷🇴" },
+        { country: "Russia", flag: "🇷🇺" },
+        { country: "Rwanda", flag: "🇷🇼" },
+        { country: "Saudi Arabia", flag: "🇸🇦" },
+        { country: "Senegal", flag: "🇸🇳" },
+        { country: "Serbia", flag: "🇷🇸" },
+        { country: "Singapore", flag: "🇸🇬" },
+        { country: "Slovakia", flag: "🇸🇰" },
+        { country: "Slovenia", flag: "🇸🇮" },
+        { country: "South Africa", flag: "🇿🇦" },
+        { country: "South Korea", flag: "🇰🇷" },
+        { country: "Spain", flag: "🇪🇸" },
+        { country: "Sri Lanka", flag: "🇱🇰" },
+        { country: "Sudan", flag: "🇸🇩" },
+        { country: "Sweden", flag: "🇸🇪" },
+        { country: "Switzerland", flag: "🇨🇭" },
+        { country: "Syria", flag: "🇸🇾" },
+        { country: "Taiwan", flag: "🇹🇼" },
+        { country: "Tajikistan", flag: "🇹🇯" },
+        { country: "Tanzania", flag: "🇹🇿" },
+        { country: "Thailand", flag: "🇹🇭" },
+        { country: "Tunisia", flag: "🇹🇳" },
+        { country: "Turkey", flag: "🇹🇷" },
+        { country: "Turkmenistan", flag: "🇹🇲" },
+        { country: "Uganda", flag: "🇺🇬" },
+        { country: "Ukraine", flag: "🇺🇦" },
+        { country: "United Arab Emirates", flag: "🇦🇪" },
+        { country: "United Kingdom", flag: "🇬🇧" },
+        { country: "United States", flag: "🇺🇸" },
+        { country: "Uruguay", flag: "🇺🇾" },
+        { country: "Uzbekistan", flag: "🇺🇿" },
+        { country: "Venezuela", flag: "🇻🇪" },
+        { country: "Vietnam", flag: "🇻🇳" },
+        { country: "Yemen", flag: "🇾🇪" },
+        { country: "Zambia", flag: "🇿🇲" },
+        { country: "Zimbabwe", flag: "🇿🇼" }
+      ];      
+}
+
+function optionsGen(method, body) {
+    return {method: method, body: JSON.stringify(body), headers: {"content-type": "application/json"}};
+}
+
+driver();
---
+++ server/server.js
@@ -0,0 +1,98 @@
+async function handler(req) {
+    const headersOBJ = new Headers();
+    headersOBJ.set("Access-Control-Allow-Origin", "*");
+    headersOBJ.set("Access-Control-Allow-Methods", "POST, DELETE, OPTIONS");
+    headersOBJ.set("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") {
+        return new Response(null, {headers: headersOBJ});
+    }
+
+    const url = new URL(req.url);
+
+    if (url.pathname === "/stage1") {
+        if (req.method === "GET") {
+            let teams = Deno.readTextFileSync("../db/stage-1.json");
+            headersOBJ.set("Content-Type", "application/json");
+            return new Response(teams, {headers: headersOBJ});
+        } else if (req.method === "POST") {
+            if (req.headers.get("content-type") !== "application/json") {return new Response("Invalid Content-Type, JSON Expected", {status: 405, headers: headersOBJ})};
+            let reqBody = await req.json();
+            if (!reqBody.team || reqBody.player1.length === 0 || reqBody.player2.length === 0 || reqBody.player3.length === 0 || reqBody.player4.length === 0 || reqBody.player5.length === 0) {
+                return new Response("Invalid request body, one or more attributes missing", {status: 400, headers: headersOBJ});
+            }
+
+            let obj = {
+                team: reqBody.team,
+                player1: reqBody.player1,
+                player2: reqBody.player2,
+                player3: reqBody.player3,
+                player4: reqBody.player4,
+                player5: reqBody.player5,
+            }
+
+            let teams = JSON.parse(Deno.readTextFileSync("../db/stage-1.json"));
+            if (teams.length === 16) {return new Response("Can't add more teams", {status: 409, headers: headersOBJ})};
+            teams.push(obj);
+            Deno.writeTextFileSync("../db/stage-1.json", JSON.stringify(teams));
+            return new Response("Team successfully added", {status: 200, headers: headersOBJ});
+        }
+    } else if (url.pathname === "/stage2") {
+        if (req.method === "GET") {
+            let teams = Deno.readTextFileSync("../db/stage-2.json");
+            headersOBJ.set("Content-Type", "application/json");
+            return new Response(teams, {headers: headersOBJ});
+        } else if (req.method === "POST") {
+            if (req.headers.get("content-type") !== "application/json") {return new Response("Invalid Content-Type, JSON Expected", {status: 405, headers: headersOBJ})};
+            let reqBody = await req.json();
+            if (!reqBody.team || reqBody.player1.length < 2 || reqBody.player2.length < 2 || reqBody.player3.length < 2 || reqBody.player4.length < 2 || reqBody.player5.length < 2) {
+                return new Response("Invalid request body, one or more attributes missing", {status: 400, headers: headersOBJ});
+            }
+
+            let obj = {
+                team: reqBody.team,
+                player1: reqBody.player1,
+                player2: reqBody.player2,
+                player3: reqBody.player3,
+                player4: reqBody.player4,
+                player5: reqBody.player5,
+            }
+
+            let teams = JSON.parse(Deno.readTextFileSync("../db/stage-2.json"));
+            if (teams.length === 16) {return new Response("Can't add more teams", {status: 409, headers: headersOBJ})};
+            teams.push(obj);
+            Deno.writeTextFileSync("../db/stage-2.json", JSON.stringify(teams));
+            return new Response("Team successfully added", {status: 200, headers: headersOBJ});
+        }
+    } else if (url.pathname === "/stage3") {
+        if (req.method === "GET") {
+            let teams = Deno.readTextFileSync("../db/stage-3.json");
+            headersOBJ.set("Content-Type", "application/json");
+            return new Response(teams, {headers: headersOBJ});
+        } else if (req.method === "POST") {
+            if (req.headers.get("content-type") !== "application/json") {return new Response("Invalid Content-Type, JSON Expected", {status: 405, headers: headersOBJ})};
+            let reqBody = await req.json();
+            if (!reqBody.team || reqBody.player1.length < 2 || reqBody.player2.length < 2 || reqBody.player3.length < 2 || reqBody.player4.length < 2 || reqBody.player5.length < 2) {
+                return new Response("Invalid request body, one or more attributes missing", {status: 400, headers: headersOBJ});
+            }
+
+            let obj = {
+                team: reqBody.team,
+                player1: reqBody.player1,
+                player2: reqBody.player2,
+                player3: reqBody.player3,
+                player4: reqBody.player4,
+                player5: reqBody.player5,
+            }
+
+            let teams = JSON.parse(Deno.readTextFileSync("../db/stage-3.json"));
+            if (teams.length === 16) {return new Response("Can't add more teams", {status: 409, headers: headersOBJ})};
+            teams.push(obj);
+            Deno.writeTextFileSync("../db/stage-3.json", JSON.stringify(teams));
+            return new Response("Team successfully added", {status: 200, headers: headersOBJ});
+        }
+    }
+
+    return new Response("Invalid Request", {status: 400, headers: headersOBJ});
+}
+
+Deno.serve(handler);
